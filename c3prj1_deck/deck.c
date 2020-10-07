c8b89ec4345969cfa0adbb43818c64cf0fa676cd
mahmoudjs14 <mahmoud.jahanshahi@yahoo.com>
2020-10-07 14:00:10 +0000

    reduced the shuffle count to n_cards/2

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -19,7 +19,7 @@ int deck_contains(deck_t * d, card_t c) {
 }
 
 void shuffle(deck_t * d){
-  for (int i=0; i<50;i++){
+  for (int i=0; i<(*d).n_cards/2;i++){
     size_t a = rand()%(*d).n_cards;
     size_t b = rand()%(*d).n_cards;
     card_t* tmp = (*d).cards[a];
