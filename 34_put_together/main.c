cf7e955e387546513a57840b24945f7431a51e8e
mahmoudjs14 <mahmoud.jahanshahi@yahoo.com>
2020-11-26 13:30:39 +0000

    did assignment 34

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -6,33 +6,48 @@
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  counts_t* countFile = createCounts();
+  FILE* f = fopen(filename, "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    exit(EXIT_FAILURE);
+  }
+  char* line = NULL;
+  size_t sz;
+  while (getline(&line, &sz, f) >= 0) {
+    char* newLine = strchr(line, '\n');
+    *newLine = '\0';
+    char* name = lookupValue(kvPairs, line);
+    addCount(countFile, name);
+    free(line);
+    line = NULL;
+  }
+  free(line);
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    exit(EXIT_FAILURE);
+  }
+  return countFile;
 }
 
-int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
-
- //count from 2 to argc (call the number you count i)
-
-    //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
-
-    //compute the output file name from argv[i] (call this outName)
-
-
-    //open the file named by outName (call that f)
-
-    //print the counts from c into the FILE f
-
-    //close f
-
-    //free the memory for outName and c
-
-
-
- //free the memory for kv
-
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    perror("Too few input arguments!");
+    return EXIT_FAILURE;
+  }
+  kvarray_t* kv = readKVs(argv[1]);
+  for (size_t i = 2; i < argc; i++) {
+    counts_t* c = countFile(argv[i], kv);
+    char* outName = computeOutputFileName(argv[i]);
+    FILE* f = fopen(outName, "w");
+    if (f == NULL) {
+      perror("Could not open file");
+      return EXIT_FAILURE;
+    }
+    printCounts(c, f);
+    freeCounts(c);
+    free(outName);
+  }
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
