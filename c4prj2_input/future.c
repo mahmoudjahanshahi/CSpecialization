e3e322fe82fe6029ec7cece6c9719ac062e6adb7
mahmoudjs14 <mahmoud.jahanshahi@yahoo.com>
2020-12-01 17:47:15 +0000

    c4prj2 attempt 8

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -3,6 +3,14 @@
 #include "future.h"
 
 void add_future_card(future_cards_t* fc, size_t index, card_t* ptr) {
+  if (fc->n_decks < (index+1)) {
+    fc->decks = realloc(fc->decks, (index+1) * sizeof(*fc->decks));
+    for (size_t i = fc->n_decks; i < index + 1; i++) {
+      fc->decks[i].cards = NULL;
+      fc->decks[i].n_cards = 0;
+      fc->n_decks++;
+    }
+  }
   fc->decks[index].cards = realloc(fc->decks[index].cards, (fc->decks[index].n_cards + 1) * sizeof(*fc->decks[index].cards));
   fc->decks[index].cards[fc->decks[index].n_cards] = ptr;
   fc->decks[index].n_cards++;
